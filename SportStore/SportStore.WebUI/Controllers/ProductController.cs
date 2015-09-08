66fcd422d4417a447b4505525592b5fa123d1a47
Ivan Tochev <annihilatora@gmail.com>
2015-09-09 00:51:26 +0300

    Basic UI added, pagination, routing

--- SportStore/SportStore.WebUI/Controllers/ProductController.cs
+++ SportStore/SportStore.WebUI/Controllers/ProductController.cs
@@ -5,21 +5,36 @@ using System.Web;
 using System.Web.Mvc;
 using SportStore.Domain.Entities;
 using SportStore.Domain.Abstract;
+using SportStore.WebUI.Models;
 
 namespace SportStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductsRepository reposiotory;
+        public int pageSize = 4;
 
         public ProductController(IProductsRepository productRepository)
         {
             this.reposiotory = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(reposiotory.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = reposiotory.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = reposiotory.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
