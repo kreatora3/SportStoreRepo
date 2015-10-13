feb59d11cb0636fbffd57efed07271ad70ca6a12
Ivan Tochev <annihilatora@gmail.com>
2015-10-14 00:55:13 +0300

    Administration functions added

---
+++ SportStore/SportStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductsRepository repository;
+
+        public AdminController(IProductsRepository repo)
+        {
+            this.repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Edit (int productID)
+        {
+          Product product = repository.Products
+              .FirstOrDefault(p => p.ProductID == productID);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
+        [HttpPost]
+
+        public ActionResult Delete(int productId)
+        {
+            Product deletedProduct = repository.DeleteProduct(productId);
+            if (deletedProduct != null)
+            {
+                TempData["message"] = string.Format("{0} was deleted", deletedProduct.Name);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
